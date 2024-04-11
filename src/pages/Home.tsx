8a04a37dd0b2c77d906a267541e43e8c0b710129
Max Hallgren <144932366+MaxHGit@users.noreply.github.com>
2024-04-11 11:57:23 +0200

    Added a section component to display cars on the homepage

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,46 @@
+import { Container } from "react-bootstrap";
+import Section from "../components/Section";
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <Container>
+      <Section
+        title={"Model 3"}
+        description={"Leasing starting at $349/mo"}
+        bgImg={"Homepage-Model-3-Desktop-LHD.jpeg"}
+        LButton={"View inventory"}
+        RButton={"Custom Order"}
+      />
+      <Section
+        title={"Model Y"}
+        description={"Leasing starting at $349/mo"}
+        bgImg={"Homepage-Model-Y-Global-Desktop.jpeg"}
+        LButton={"View inventory"}
+        RButton={"Custom Order"}
+      />
+      <Section
+        title={"Model S"}
+        description={"Leasing starting at $349/mo"}
+        bgImg={"Model-S-homepage-desktop.jpeg"}
+        LButton={"View inventory"}
+        RButton={"Custom Order"}
+      />
+      <Section
+        title={"Model X"}
+        description={"Leasing starting at $349/mo"}
+        bgImg={"Homepage-Model-X-Desktop-LHD.jpeg"}
+        LButton={"View inventory"}
+        RButton={"Custom Order"}
+      />
+      <Section
+        title={"Solar Roof"}
+        description={"Leasing starting at $349/mo"}
+        bgImg={"Homepage-SolarRoof-Desktop-Global.webp"}
+        LButton={"View inventory"}
+        RButton={"Custom Order"}
+      />
+    </Container>
+  );
 };
 
 export default Home;
